357261cc1ec4bf53cd463f205763c19ad5315ffd
Gabriel Shoyombo <gabrielshoyombo2002@gmail.com>
2022-08-10 04:25:26 +0100

    Created a tweet template

--- src/index.js
+++ src/index.js
@@ -1,18 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-function Hello(){
+function Tweet() {
   return (
-    <div>Hello, world</div>
+    <div className='tweet'>
+      <Avatar />
+      <div className='content'>
+        <NameWithHandle />
+        <Message />
+        <Buttons />
+      </div>
+    </div>
   )
 }
 
-ReactDOM.render(<Hello />, document.getElementById("root"))
+function Avatar() {
+  return (
+    <img src='https://gravatar.com/avatar/nothing'
+    className='avatar'
+    alt='avatar' />
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Message() {
+  return (
+    <div className='message'>
+      This is less than 140 characters
+    </div>
+  )
+}
+
+function NameWithHandle() {
+  return (
+    <div className='name-with-handle'>
+      <span className='name'>Your Name</span>
+      <span className='handle'>@yourhandle</span>
+      <Time/>
+    </div>
+  )
+}
+
+function Buttons() {
+  return (
+    <div className='buttons'>
+      <i className='fa fa-comment comment-btn' />
+      <i className='fa fa-retweet retweet-btn' />
+      <i className='fa fa-heart like-btn'/>
+      <i className='fa fa-ellipsis-h more-options-btn'/>
+    </div>
+  )
+}
+
+function Time() {
+  return (
+    <span className='time'>3h ago</span> 
+  )
+}
+
+ReactDOM.render(<Tweet />, document.querySelector("#root"))
